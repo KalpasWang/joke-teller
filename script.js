be675f9327c9df619c4de590d6d1515867184d08
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-08-17 23:04:41 +0800

    ui redesign

--- script.js
+++ script.js
@@ -1,37 +1,100 @@
-const button = document.getElementById('button');
-const synth = window.speechSynthesis;
-const responseText = document.querySelector('#response-text');
+const synth = window.speechSynthesis;  // speech synthesis api
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;  // speech recognition api
+const speakBtn = document.getElementById('speak-btn');
+const text = document.getElementById('text');
+const headImg = document.getElementById('head');
+const beepSound = new Audio('beep.mp3');
+let nomatch = false;  // set to true if no words match in speech recognition
+
+
+
+// test if this browser has sppech recognition api
+if(!SpeechRecognition){
+  alert('Sorry, your browser does not support this feature!')
+} else {
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;  // result event will be triggered every word
+  recognition.lang = 'en-US';
+  
+  recognition.addEventListener('start', () => {
+    nomatch = true; // set nomatch to true at beginning
+    toggleButton();
+  });
+
+  
+  recognition.addEventListener('result', e => {
+    // get transcript from event object
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)[0];
+
+    text.textContent = transcript; // update text in html
+    nomatch = false; // if result event is fired, nomatch set to false
+  });
+
+
+  recognition.addEventListener('end', () => {
+    console.log('Speech recognition has stopped.');
+    if(nomatch) {
+      text.textContent = 'Ask for a joke!';
+      toggleButton();
+    } else if(text.textContent.includes('joke')) {
+      getJoke();
+    } else {
+      speak("Sorry, I don't understand")
+    }
+  });
+  
+  speakBtn.addEventListener('click', () => {
+    console.log('Voice recognition started. Try speaking into the microphone.');
+    
+    beepSound.play();
+    recognition.start();
+  });
+}
 
 
 // Disable/Enable Button
 function toggleButton() {
-  button.disabled = !button.disabled;
+  speakBtn.disabled = !speakBtn.disabled;
+}
+
+// change image to talking head or silent head
+function toggleHead() {
+  if(headImg.src.split('/').pop() === 'head.png') {
+    headImg.src = 'talking-head.gif';
+  } else {
+    headImg.src = 'head.png';
+  }
 }
 
 // Speech Function
-const speakJoke = (jokeString) => {
+const speak = (speakingText) => {
   // Check if speaking
   if (synth.speaking) {
     return;
   }
 
   // Get speak text
-  const speakText = new SpeechSynthesisUtterance(jokeString);
-  console.log(jokeString);
-  responseText.textContent = jokeString;
+  const utterText = new SpeechSynthesisUtterance(speakingText);
+  const voices = synth.getVoices();
+  utterText.voice = voices.find(voice => voice.lang === 'en-US');
+  text.textContent = speakingText;
 
   // Speak end
-  speakText.onend = e => {
+  utterText.onend = e => {
+    toggleHead();
     toggleButton();
   };
 
   // Speak error
-  speakText.onerror = e => {
+  utterText.onerror = e => {
     console.error('Something went wrong');
   };
 
   // Speak
-  synth.speak(speakText);
+  toggleHead()
+  synth.speak(utterText);
 };
 
 // Get jokes from Joke API
@@ -47,14 +110,8 @@ async function getJoke() {
     } else {
       joke = data.joke;
     }
-    // Passing Joke to VoiceRSS API
-    speakJoke(joke);
-    // Disable Button
-    toggleButton();
+    speak(joke);
   } catch (error) {
     // Catch Error Here
   }
 }
-
-// Event Listeners
-// button.addEventListener('click', getJokes);
